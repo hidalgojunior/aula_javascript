48a12a44eb3c37a2e03b4dff0ee1b1ff82b942eb
hidalgojunior <arnaldo@hidalgojunior.com.br>
2021-03-31 03:31:28 -0300

    Final da aula e do curso

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-alert("Meu primeiro JS");
+// var lista = ["maça", "pera", "laranja"];
+
+// console.log(lista);
+// console.log(lista[1]);
+// alert(lista[1]);
+
+// lista.push("uva");
+// alert(lista);
+// lista.pop();
+// alert(lista);
+
+// var
+
+// function soma(n1, n2) {
+//     return n1 + n2;
+// }
+
+function clicou() {
+    console.log(document.getElementById("agradecimento"));
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar...!!!";
+}
+
+function redirecionar() {
+    window.open("http://www.devisate.com.br");
+}
